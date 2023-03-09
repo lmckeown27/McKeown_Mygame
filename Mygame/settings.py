232b46d6b28fa09df561773079c93210e98163ab
Liam McKeown <121582920+lmckeown27@users.noreply.github.com>
2023-03-09 12:04:30 -0800

    Add files via upload

---
+++ Mygame/settings.py
@@ -0,0 +1,16 @@
+# File created by: Liam McKeown
+
+# screen dimensions
+WIDTH = 800
+HEIGHT = 600
+
+# game settings:
+FPS = 30
+RUNNING = True
+
+# player attributes
+PLAYER_ACC = 2
+PLAYER_FRICTION = -0.12
+# colors
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
